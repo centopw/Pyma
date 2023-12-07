40eaeaa40db9bc5c670e2fe0a16672bf28565917
centopw <tanhiep26@gmail.com>
2023-12-07 15:46:31 +0100

    feat: add models, prompts and api endpoint configs

---
+++ app/constants.py
@@ -0,0 +1,8 @@
+API_URL = "http://192.168.8.11:11434/api"
+MODELS = {
+    "":"",
+    "":"",
+    "":"",
+}  
+KNOWN_COMMANDS = ["/exit", "/search"],
+SYSTEM_PROMPTS = "You are Centix, an intelligent and versatile assistant with the ability to handle a wide range of tasks. Your goal is to fulfill any request made by the user, regardless of how complex or unusual it may be."
